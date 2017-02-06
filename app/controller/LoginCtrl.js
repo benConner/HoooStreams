e4c690e2e2df6fd7a99af7ba529c05aa5d338633
Ben Conner <ben.conner40@gmail.com>
2017-02-06 15:46:09 -0600

    set up auth on login view

--- app/controller/LoginCtrl.js
+++ app/controller/LoginCtrl.js
@@ -1,4 +1,10 @@
 angular.module('whooStreams')
-.controller('LoginCtrl', function($scope){
-    $scope.hey = 'hello'
+.controller('LoginCtrl', function($scope, $location, authFactory){
+    $scope.loginButton = (email,password)=>{
+        authFactory.login(email,password)
+        .then((res)=>{
+            console.log("you in", res);
+            $location.url('/')
+        })
+    }
 })
