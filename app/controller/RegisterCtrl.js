6d42c899a03f91cab63c1a62c54f93c7f1074d28
Ben Conner <ben.conner40@gmail.com>
2017-02-06 15:53:50 -0600

    set up auth on register view

--- app/controller/RegisterCtrl.js
+++ app/controller/RegisterCtrl.js
@@ -1,4 +1,12 @@
 angular.module('whooStreams')
-.controller('RegisterCtrl', function($scope){
+.controller('RegisterCtrl', function($scope,$location, authFactory){
     console.log("RegisterCtrl");
+    $scope.registerButton = (email, password)=>{
+        authFactory.register(email, password)
+        .then((res)=>{
+            console.log("you new?", res);
+            $location.url('/')
+        })
+    }
+
 })
