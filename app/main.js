4d35a164d3c6b4faeadeee9a2b44b71a933686e6
Ben Conner <ben.conner40@gmail.com>
2017-02-06 15:54:31 -0600

    firebase initialized

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
 angular.module('whooStreams',['ngRoute']);
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyADn1Z_S7hqeFemR7a5s61_ec2CHNahxN0",
+    authDomain: "whoostreams.firebaseapp.com",
+    databaseURL: "https://whoostreams.firebaseio.com",
+    storageBucket: "whoostreams.appspot.com",
+    messagingSenderId: "913361497296"});
