6c2dd1749185af2450845ceaa5610bc5fdc3b15e
Ben Conner <ben.conner40@gmail.com>
2017-02-07 11:52:50 -0600

    calling api for movie data

--- app/controller/MainCtrl.js
+++ app/controller/MainCtrl.js
@@ -1,4 +1,17 @@
 angular.module('whooStreams')
-.controller('MainCtrl', function($scope, authFactory){
+.controller('MainCtrl', function($scope, authFactory, guideboxFactory){
     $scope.logOutButton = authFactory.logOut
+    guideboxFactory.loadinMovie()
+    .then((res)=>{
+        $scope.movies = res
+    })
+    $scope.find = (content)=>{
+        console.log("content", content);
+        guideboxFactory.findMovie(content)
+        .then((res)=>{
+            console.log("res", res);
+            $scope.movies = res;
+        })
+        $scope.content = "";
+    }
 })
