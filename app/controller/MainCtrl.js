1c8a93cf9680a71e9cbbec4013bac84d12cbbfc7
Ben Conner <ben.conner40@gmail.com>
2017-02-08 14:48:33 -0600

    call by movie id to get streaming info

--- app/controller/MainCtrl.js
+++ app/controller/MainCtrl.js
@@ -1,17 +1,30 @@
 angular.module('whooStreams')
 .controller('MainCtrl', function($scope, authFactory, guideboxFactory){
+
     $scope.logOutButton = authFactory.logOut
+
     guideboxFactory.loadinMovie()
     .then((res)=>{
+        res
         $scope.movies = res
     })
     $scope.find = (content)=>{
         console.log("content", content);
         guideboxFactory.findMovie(content)
         .then((res)=>{
-            console.log("res", res);
             $scope.movies = res;
         })
         $scope.content = "";
+    }
+    $scope.movieId = (id)=>{
+        console.log("id", id);
+        guideboxFactory.findMovieByID(id)
+        .then((res)=>{
+            console.log("res", res);
+            $scope.frees = res.free_web_sources
+            $scope.subs = res.subscription_web_sources
+            $scope.purchases = res.purchase_web_sources
+
+        })
     }
 })
--- app/factories/guideboxFactory.js
+++ app/factories/guideboxFactory.js
@@ -3,16 +3,24 @@ angular.module('whooStreams')
     return{
         loadinMovie : ()=>{
             return $http
-            .get('http://api-public.guidebox.com/v2/movies?api_key=801414ed2250b78cfd77530a81236699a890f6ec ')
+            .get('http://api-public.guidebox.com/v2/movies?api_key=801414ed2250b78cfd77530a81236699a890f6ec&limit=24')
             .then((res)=>{
                 return res.data.results;
             })
         },
         findMovie : (title)=>{
             return $http
-            .get(`http://api-public.guidebox.com/v2/search?api_key=801414ed2250b78cfd77530a81236699a890f6ec&type=movie&field=title&query=${title}`)
+            .get(`http://api-public.guidebox.com/v2/search?api_key=801414ed2250b78cfd77530a81236699a890f6ec&type=movie&field=title&limit=5&query=${title}`)
             .then((res)=>{
-                return res.data.results;
+                return res.data.results.slice(0,9);
+            })
+        },
+        findMovieByID : (id)=>{
+            return $http
+            .get(`http://api-public.guidebox.com/v2/movies/${id}?api_key=801414ed2250b78cfd77530a81236699a890f6ec&sources=free,amazon_prime,netflix,hulu,hbo,showtime,stars,i_tune,google_play,cinemax,vudu`)
+            .then((res)=>{
+                //console.log("res movie id", res);
+                return res.data;
             })
         }
     }
