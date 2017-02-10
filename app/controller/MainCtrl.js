2a63f3300adddd44bcb5d3a73c3d4380818389a2
Ben Conner <ben.conner40@gmail.com>
2017-02-10 13:43:40 -0600

    added movie to search and display

--- app/controller/MainCtrl.js
+++ app/controller/MainCtrl.js
@@ -1,30 +1,63 @@
 angular.module('whooStreams')
-.controller('MainCtrl', function($scope, authFactory, guideboxFactory){
-
+.controller('MainCtrl', function($scope, authFactory, guideboxMovieFactory,
+    guideboxShowFactory){
+    $('#modal1').modal('');
+    $('ul.tabs').tabs();
     $scope.logOutButton = authFactory.logOut
 
-    guideboxFactory.loadinMovie()
+    guideboxMovieFactory.loadinMovie()
     .then((res)=>{
-        res
+        console.log("loadin res", res);
         $scope.movies = res
     })
-    $scope.find = (content)=>{
+
+    guideboxShowFactory.loadinShows()
+    .then((res)=>{
+        console.log("loadinshow res", res);
+        $scope.shows = res
+    })
+    $scope.findMovie = (content)=>{
         console.log("content", content);
-        guideboxFactory.findMovie(content)
+        guideboxMovieFactory.findMovie(content)
         .then((res)=>{
             $scope.movies = res;
+            $scope.findShow(content)
         })
-        $scope.content = "";
     }
     $scope.movieId = (id)=>{
         console.log("id", id);
-        guideboxFactory.findMovieByID(id)
+        guideboxMovieFactory.findMovieByID(id)
         .then((res)=>{
             console.log("res", res);
-            $scope.frees = res.free_web_sources
-            $scope.subs = res.subscription_web_sources
-            $scope.purchases = res.purchase_web_sources
-
+            $scope.title = res.title;
+            $scope.img = res.poster_240x342;
+            $scope.frees = res.free_web_sources;
+            $scope.subs = res.subscription_web_sources;
+            $scope.purchases = res.purchase_web_sources;
+            $('#modal1').modal('open')
         })
     }
+    $scope.showId = (show)=>{
+        //console.log("id", id);
+        guideboxShowFactory.findShowByID(show.id)
+        .then((res)=>{
+            console.log("res", res);
+            $scope.title = show.title;
+            $scope.img = show.artwork_208x117;
+            $scope.frees = res;
+            $scope.subs = res;
+            $scope.purchases = res;
+            $('#modal1').modal('open')
+        })
+    }
+
+    $scope.findShow = (content)=>{
+        console.log("content", content);
+        guideboxShowFactory.findShow(content)
+        .then((res)=>{
+            console.log(" show res", res);
+            $scope.shows = res;
+        })
+    }
+
 })
