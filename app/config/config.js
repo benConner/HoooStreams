ecfc659d4305ac7a559b261469ca8023dfc1bc00
Ben Conner <ben.conner40@gmail.com>
2017-02-14 13:13:02 -0600

    resolve user auth

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,10 @@ angular.module('whooStreams')
     $routeProvider
     .when('/', {
         controller: 'MainCtrl',
-        templateUrl: 'app/partials/main.html'
+        templateUrl: 'app/partials/main.html',
+        resolve: {user: function(authFactory){
+            return authFactory.showHideLogout()
+        }}
     }).when('/login',{
         controller: 'LoginCtrl',
         templateUrl: 'app/partials/login.html'
@@ -12,8 +15,11 @@ angular.module('whooStreams')
         controller: 'RegisterCtrl',
         templateUrl: 'app/partials/register.html'
     }).when('/my_media',{
-         controller: 'MyMediaCtrl',
-         templateUrl: 'app/partials/myMedia.html'
+        controller: 'MyMediaCtrl',
+        templateUrl: 'app/partials/myMedia.html',
+        resolve: {user: function(authFactory){
+            return authFactory.showHideLogout()
+        }}
       }).otherwise({
         redirectTo:'/'
     })
