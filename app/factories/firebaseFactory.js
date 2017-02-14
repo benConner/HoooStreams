97a970964bec39acc5cd22e9a9d006c2b6a259d8
Ben Conner <ben.conner40@gmail.com>
2017-02-14 13:20:16 -0600

    delete unwanted media in My Media

--- app/factories/firebaseFactory.js
+++ app/factories/firebaseFactory.js
@@ -13,9 +13,18 @@ angular.module('whooStreams')
                 console.error(res);
             })
         },
+        deleteMyMedia : (user, content)=>{
+            //
+            return $http
+            .delete(`https://whoostreams.firebaseio.com/${user}/${content}.json`)
+            .catch((res)=>{
+                console.error(res);
+            })
+        },
+        //loads all users media they saved
         loadinMyMedia : (user)=>{
             return $http
-            .get(`https://whoostreams.firebaseio.com/user.json`)
+            .get(`https://whoostreams.firebaseio.com/${user}.json`)
             .then((res)=>{
                 console.log("firebaseFactory res", res);
                 return res;
