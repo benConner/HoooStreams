b67a8f7b7ef7f47cb10b18cd549da83cb79b5add
Ben Conner <ben.conner40@gmail.com>
2017-02-27 15:17:25 -0600

    code clean up

--- app/controller/MainCtrl.js
+++ app/controller/MainCtrl.js
@@ -33,61 +33,36 @@ angular.module('whooStreams')
 
     //finds movies from title search and the does the same for shows
     $scope.findMovie = (content)=>{
-        console.log("content", content);
         guideboxMovieFactory.findMovie(content)
         .then((res)=>{
             $scope.movies = res;
             $scope.findShow(content)
         })
     }
+
     //grabs movie id and gets streaming info
-    $scope.movieId = (id)=>{
-        console.log("id", id);
-        guideboxMovieFactory.findMovieByID(id)
-        .then((res)=>{
-            $scope.media = res;
-            $scope.title = res.title;
-            $scope.img = res.poster_240x342;
-            $scope.frees = res.free_web_sources;
-            $scope.subs = res.subscription_web_sources;
-            $scope.purchases = res.purchase_web_sources;
-            $('#modal1').modal('open')
-        })
+    $scope.movieId = (movie)=>{
+        movie.type = "movies";
+        $location.url(`/media/${movie.type}/${movie.title}/${movie.id}`)
     }
     //grabs show id and gets streaming info
     $scope.showId = (show)=>{
-        guideboxShowFactory.findShowByID(show.id)
-        .then((res)=>{
-            show.type = "show";
-            console.log("show", show);
-            $scope.media = show;
-            $scope.title = show.title;
-            $scope.img = show.artwork_208x117;
-            $scope.frees = res;
-            $scope.subs = res;
-            $scope.purchases = res;
-            $('#modal1').modal('open')
-        })
+        show.type = "shows";
+        $location.url(`/media/${show.type}/${show.title}/${show.id}`)
     }
 
     // function to find shows by title
     $scope.findShow = (content)=>{
-        console.log("content", content);
         guideboxShowFactory.findShow(content)
         .then((res)=>{
-            console.log(" show res", res);
             $scope.shows = res;
         })
     }
 
-    //saves media to users firebase
-    $scope.addMedia = (uid, media)=>{
-        console.log("media", media);
-        firebaseFactory.saveToMyMedia(uid, media)
-    }
+
 
     //my media tab redirect
-    $scope.media = ()=>{
+    $scope.myMedia = ()=>{
         $location.url('/my_media')
     }
 
