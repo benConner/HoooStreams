09f385373d39362b1d43fcd34468cdfa6d4eb726
Ben Conner <ben.conner40@gmail.com>
2017-02-27 20:07:05 -0600

    linked media view and get info by show id

--- app/controller/MyMediaCtrl.js
+++ app/controller/MyMediaCtrl.js
@@ -1,40 +1,53 @@
 angular.module('whooStreams')
-.controller('MyMediaCtrl',function($scope, firebaseFactory, guideboxMovieFactory){
+.controller('MyMediaCtrl',function($scope, firebaseFactory, guideboxMovieFactory, guideboxShowFactory, user, $location){
     //materialize modal
     $('#modal1').modal('');
 
-    $scope.uid = firebase.auth().currentUser.uid;
     load()
 
     function load(){
          firebaseFactory.loadinMyMedia(firebase.auth().currentUser.uid)
         .then((res)=>{
-            console.log("res", res);
             $scope.media = res.data
         })
     }
     $scope.userId = firebase.auth().currentUser.uid;
 
-    $scope.movieId = (id, key)=>{
-        console.log("id", key);
-        guideboxMovieFactory.findMovieByID(id)
+    $scope.movieId = (movie, key)=>{
+        guideboxMovieFactory.findMovieByID(movie.id)
         .then((res)=>{
-            //$scope.key = key;
+            $scope.mediaInfo = movie;
+            $scope.key = key;
             $scope.title = res.title;
             $scope.img = res.poster_240x342;
             $scope.frees = res.free_web_sources;
             $scope.subs = res.subscription_web_sources;
-            $scope.purchases = res.purchase_web_sources;
+            $scope.srces = undefined;
+            $('#modal1').modal('open')
+        })
+    }
+    $scope.showId = (show, key)=>{
+        guideboxShowFactory.findShowByID(show.id)
+        .then((res)=>{
+            $scope.mediaInfo = show;
+            $scope.key = key;
+            $scope.title = show.title;
+            $scope.img = show.artwork_208x117;
+            $scope.frees = undefined;
+            $scope.subs = undefined;
+            $scope.srces = res;
             $('#modal1').modal('open')
         })
     }
 
     $scope.deleteMedia = (id, content)=>{
-        console.log("content", content);
         firebaseFactory.deleteMyMedia(id, content)
         .then((res)=>{
-            console.log("text", res);
             load()
         })
     }
+    $scope.mediaInfoPage = (mediaInfo)=>{
+            $location.url(`/media/${mediaInfo.type}/${mediaInfo.title}/${mediaInfo.id}/`)
+    }
+
 })
