9f222ab4d16f335d419980b7dcdfcc2c15fb6dc2
Ben Conner <ben.conner40@gmail.com>
2017-06-05 23:30:12 -0500

    login comments for readability

--- app/controller/LoginCtrl.js
+++ app/controller/LoginCtrl.js
@@ -1,5 +1,6 @@
 angular.module('whooStreams')
 .controller('LoginCtrl', function($scope, $location, authFactory){
+    // login to get profile info from firebase
     $scope.loginButton = (email,password)=>{
         authFactory.login(email,password)
         .then((res)=>{
