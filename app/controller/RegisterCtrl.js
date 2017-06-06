d26c96bc47c0394f413c939fb2fb667159a46a47
Ben Conner <ben.conner40@gmail.com>
2017-06-05 23:32:17 -0500

    register comment for readability

--- app/controller/RegisterCtrl.js
+++ app/controller/RegisterCtrl.js
@@ -1,10 +1,10 @@
 angular.module('whooStreams')
 .controller('RegisterCtrl', function($scope,$location, authFactory){
-    console.log("RegisterCtrl");
+
+    //function to create user profile
     $scope.registerButton = (email, password, username)=>{
         authFactory.register(email, password, username)
         .then((res)=>{
-            console.log("you new?", res);
             $location.url('/')
         })
     }
